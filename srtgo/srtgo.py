edd0a9b78c84e94a9b7252562cd71ed2f58cbedd
Dohoung Kim <lapis42@gmail.com>
2024-09-25 12:17:13 +0900

    Added more discount types

--- srtgo/srtgo.py
+++ srtgo/srtgo.py
@@ -513,11 +513,28 @@ def check_reservation(rail_type="SRT"):
 
 class SRTTicket2(SRTTicket):
     DISCOUNT_TYPE = {
-        '000': '어른/청소년',
-        '201': '어린이',
-        '301': '경로',
-        '205': '장애 1~3급',
-        '206': '장애 4~6급',
+        "000": "어른/청소년",
+        "101": "탄력운임기준할인",
+        "105": "자유석 할인",
+        "106": "입석 할인",
+        "107": "역방향석 할인",
+        "108": "출입구석 할인",
+        "109": "가족석 일반전환 할인",
+        "111": "구간별 특정운임",
+        "112": "열차별 특정운임",
+        "113": "구간별 비율할인(기준)",
+        "114": "열차별 비율할인(기준)",
+        "121": "공항직결 수색연결운임",
+        "131": "구간별 특별할인(기준)",
+        "132": "열차별 특별할인(기준)",
+        "133": "기본 특별할인(기준)",
+        "191": "정차역 할인",
+        "192": "매체 할인",
+        "201": "어린이",
+        "202": "동반유아 할인",
+        "204": "경로",
+        "205": "1~3급 장애인",
+        "206": "4~6급 장애인",
     }
     
     def __init__(self, data):
@@ -529,7 +546,7 @@ class SRTTicket2(SRTTicket):
         if self.passenger_type_code in self.DISCOUNT_TYPE:
             self.passenger_type = self.DISCOUNT_TYPE[self.passenger_type_code]
         else:
-            self.passenger_type = '어른/청소년'
+            self.passenger_type = '기타 할인'
 
         self.price = int(data["rcvdAmt"])
         self.original_price = int(data["stdrPrc"])
